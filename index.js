44d97dd083a2f76c2815fe2d01b44f41a5fdbddc
Likhon-baRoy <likhonhere007@proton.me>
2025-05-17 23:34:12 +0600

    install timer, result calculation, start on click

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ let homeScore = 0;
 let guestScore = 0;
 let homeEl = document.getElementById("home-el");
 let guestEl = document.getElementById("guest-el");
-let timer = document.getElementById("g");
-let homeOne = document.getElementById("home-one");
+let timer = document.getElementById("timer");
+
+let countdownInterval = null;
 
 function addScore(team, points) {
+    if (countdownInterval === null) {
+        alert("First you have to START NEW GAME")
+        return;
+    }
+
     if(team === "home") {
         homeScore += points;
         homeEl.innerText = homeScore;
@@ -15,14 +21,41 @@ function addScore(team, points) {
     }
 }
 
-// if(homeScore > guestScore) {
+function startCountdown(seconds) {
+    let timeLeft = seconds;
+    updateTimerDisplay(timeLeft);
 
-// }
+    countdownInterval = setInterval(() => {
+        timeLeft--;
+        updateTimerDisplay(timeLeft);
 
+        if (timeLeft <= 0) {
+            clearInterval(countdownInterval);
+            countdownInterval = null;
+            declearWinner();
+        }
+    }, 1000);
+}
+
+function updateTimerDisplay(seconds) {
+    let display = seconds < 10 ? `00:0${seconds}` : `00:${seconds}`;
+    timer.innerText = display;
+}
+
+function declearWinner() {
+    if (homeScore > guestScore) {
+        timer.innerText = "Home Won!";
+    } else if (homeScore < guestScore) {
+        timer.innerText = "Guest Won!";
+    } else {
+        timer.innerHTML = "It's a Tie :)";
+    }
+}
 
 let reset = document.getElementById("resetGame").addEventListener("click", () => {
     homeScore = 0;
     guestScore = 0;
     homeEl.innerText = 0;
     guestEl.innerText = 0;
+    startCountdown(10);
 });
